01f069716cc91b16d8a72ceeec689938d48c0654
krystal <1539962101@qq.com>
2023-10-08 22:26:03 +0800

    nigth2day_changeConfig

--- datasets/common.py
+++ datasets/common.py
@@ -7,7 +7,7 @@ ROBOTCAR_ROOT = {
     'night': '../data/RobotCar-Night/2014-12-16-18-44-24'
 }
 NUSCENES_ROOT = {
-    'sequence': '/data/zs_data/nuScenes/train_output_night/sequences',
+    'sequence': '/data/zs_data/nuScenes/train_output_day/sequences',
     'test_color': '/data/zs_data/nuScenes-Night/test/color',
     'test_gt': '/data/zs_data/nuScenes-Night/test/gt',
     'split': '/home/zs/RNW-master/split_files/nuscenes'
--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-CONFIG=rnw_ns
+CONFIG=mono2_ns_day
 GPUS=2
 
 python3 train.py $CONFIG $GPUS
